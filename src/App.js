a7a91b09298e21d1275f82727065c425ec6a756c
Pedro Marques <pedromarques1011@gmail.com>
2020-11-18 17:05:32 -0300

    1-6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
-import Home from './Home';
+import React, { Component } from "react";
+import Home from "./Home";
+import About from "./About";
+import Users from "./Users";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Users" component={Users} />
+      </Router>
     );
   }
 }
